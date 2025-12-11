8291bfd38581b07b0bd9a2a9ed30c25fab162db2
sumithaha <no@email.com>
2025-12-11 19:07:34 +0530

    test2

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const API_BASE = import.meta.env.VITE_BACKEND_URL;
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: API_BASE,
   withCredentials: true,
 });
